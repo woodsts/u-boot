0f53732201b97b79d36e0e26688b1f05ea6151b5
Tom Rini <trini@konsulko.com>
2022-12-02 21:03:41 -0500

    global: Migrate CONFIG_PRAM to CFG
    Perform a simple rename of CONFIG_PRAM to CFG_PRAM
    
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/km/km-powerpc.h
+++ include/configs/km/km-powerpc.h
@@ -25,7 +25,7 @@
 
 /* set the default PRAM value to at least PNVRAM + PHRAM when pram env variable
  * is not valid yet, which is the case for when u-boot copies itself to RAM */
-#define CONFIG_PRAM		((CONFIG_KM_PNVRAM + CONFIG_KM_PHRAM)>>10)
+#define CFG_PRAM		((CONFIG_KM_PNVRAM + CONFIG_KM_PHRAM)>>10)
 
 /* architecture specific default bootargs */
 #define CFG_KM_DEF_BOOT_ARGS_CPU		""
