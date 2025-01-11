b1e0f8a9f428e0f9a049f064f0994fe7c1ac849c
Simon Glass <sjg@chromium.org>
2025-01-10 17:00:05 -0700

    serial: Support info() method in ns16550 xPL with UPL
    UPL needs to pass the serial details onto the next stage, so adjust the
    condition to support this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- drivers/serial/ns16550.c
+++ drivers/serial/ns16550.c
@@ -473,7 +473,7 @@ static int ns16550_serial_getinfo(struct udevice *dev,
 	struct ns16550_plat *plat = com_port->plat;
 
 	/* save code size */
-	if (!not_xpl())
+	if (!not_xpl() && !CONFIG_IS_ENABLED(UPL_OUT))
 		return -ENOSYS;
 
 	info->type = SERIAL_CHIP_16550_COMPATIBLE;
