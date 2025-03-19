sjg20/u-boot-concept

test/py: Rework test_cat to not use virt-make-fs
The problem with using "virt-make-fs" to make a filesystem image is that
it is extremely slow. Switch to using the fs_helper functions we have
instead from the filesystem tests as these can add files to images and
are significantly faster and still do not require root access.

Signed-off-by: Tom Rini <trini@konsulko.com>

test/py/tests/test_cat.py
@@ -1,0 +1,37 @@
+# SPDX-License-Identifier:      GPL-2.0+
+
+""" Unit test for cat command
+"""
+
+import pytest
+from subprocess import call, check_call, CalledProcessError
+from tests import fs_helper
+
+@pytest.mark.boardspec('sandbox')
+@pytest.mark.buildconfigspec('cmd_cat')
+def test_cat(u_boot_console):
+    """ Unit test for cat
+
+    Args:
+        u_boot_console -- U-Boot console
+    """
+    try:
+        scratch_dir = u_boot_console.config.persistent_data_dir + '/scratch'
+
+        check_call('mkdir -p %s' % scratch_dir, shell=True)
+
+        with open(scratch_dir + '/hello', 'w', encoding = 'ascii') as file:
+            file.write('hello world\n')
+
+        cat_data = fs_helper.mk_fs(u_boot_console.config, 'vfat', 0x100000,
+                                   'test_cat', scratch_dir)
+        response = u_boot_console.run_command_list([ f'host bind 0 {cat_data}',
+                                                    'cat host 0 hello'])
+        assert 'hello world' in response
+    except CalledProcessError as err:
+        pytest.skip('Preparing test_cat image failed')
+        call('rm -f %s' % cat_data, shell=True)
+        return
+    finally:
+        call('rm -rf %s' % scratch_dir, shell=True)
+        call('rm -f %s' % cat_data, shell=True)

woodsts/u-boot

test/py/tests/test_cat.py
@@ -1,0 +1,37 @@
+# SPDX-License-Identifier:      GPL-2.0+
+
+""" Unit test for cat command
+"""
+
+import pytest
+from subprocess import call, check_call, CalledProcessError
+from tests import fs_helper
+
+@pytest.mark.boardspec('sandbox')
+@pytest.mark.buildconfigspec('cmd_cat')
+def test_cat(ubman):
+    """ Unit test for cat
+
+    Args:
+        ubman -- U-Boot console
+    """
+    try:
+        scratch_dir = ubman.config.persistent_data_dir + '/scratch'
+
+        check_call('mkdir -p %s' % scratch_dir, shell=True)
+
+        with open(scratch_dir + '/hello', 'w', encoding = 'ascii') as file:
+            file.write('hello world\n')
+
+        cat_data = fs_helper.mk_fs(ubman.config, 'vfat', 0x100000,
+                                   'test_cat', scratch_dir)
+        response = ubman.run_command_list([ f'host bind 0 {cat_data}',
+                                                    'cat host 0 hello'])
+        assert 'hello world' in response
+    except CalledProcessError as err:
+        pytest.skip('Preparing test_cat image failed')
+        call('rm -f %s' % cat_data, shell=True)
+        return
+    finally:
+        call('rm -rf %s' % scratch_dir, shell=True)
+        call('rm -f %s' % cat_data, shell=True)