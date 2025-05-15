af5b3ce79eca72dd52848486fc4a56674995a4e4
Lukasz Czechowski <lukasz.czechowski@thaumatec.com>
2025-05-15 17:55:45 +0200

    debug_uart: Replace debug functions with dummies if CONFIG_DEBUG_UART is not set
    In case DEBUG UART is not used, define dummy macros replacing
    the actual function implementations that will not be available.
    This allows to compile code and avoid linker errors.
    Redefine the DEBUG_UART_FUNCS macro if DEBUG UART is not available,
    to avoid compilation errors.
    
    Signed-off-by: Lukasz Czechowski <lukasz.czechowski@thaumatec.com>

--- include/debug_uart.h
+++ include/debug_uart.h
@@ -128,6 +128,8 @@ void printdec(unsigned int value);
 			(1 << CONFIG_DEBUG_UART_SHIFT), \
 		CONFIG_DEBUG_UART_SHIFT)
 
+#ifdef CONFIG_DEBUG_UART
+
 /*
  * Now define some functions - this should be inserted into the serial driver
  */
@@ -196,5 +198,18 @@ void printdec(unsigned int value);
 		_debug_uart_init(); \
 		_DEBUG_UART_ANNOUNCE \
 	} \
+
+#else
+
+#define DEBUG_UART_FUNCS
+
+#define printch(ch) (void)(ch)
+#define printascii(str) (void)(str)
+#define printhex2(value) (void)(value)
+#define printhex4(value) (void)(value)
+#define printhex8(value) (void)(value)
+#define printdec(value) (void)(value)
+
+#endif
 
 #endif
